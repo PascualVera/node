5048f80ad2430fd98c50191e415857a3122a4fa1
PascualVera <pascualvera.994@gmail.com>
2022-02-09 16:05:48 +0100

    carpeta añadida

---
+++ dia2/node_proyect_async/writeJson.js
@@ -0,0 +1,9 @@
+import * as fs from "fs/promises"
+
+export const write = async (path, object) => {
+	await fs.writeFile(path, JSON.stringify(object))
+
+	const data = await fs.readFile(path, "utf8")
+
+	console.log(JSON.parse(data))
+}
