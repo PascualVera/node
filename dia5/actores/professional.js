f1e114bba65408f061c003096b61bc42a03846e6
PascualVera <pascualvera.994@gmail.com>
2022-02-14 12:50:31 +0100

    Database actors and pokemon

---
+++ dia5/actores/professional.js
@@ -0,0 +1,20 @@
+class Professional {
+	constructor() {
+		this.name = ""
+		this.age = ""
+		this.genre = ""
+		this.weight = ""
+		this.height = ""
+		this.hairColor = ""
+		this.race = ""
+		this.isRetired = ""
+		this.nationality = ""
+		this.oscarsNumber = ""
+		this.profesion = ""
+		this.foto = ""
+	}
+}
+
+module.exports = {
+	Professional,
+}
